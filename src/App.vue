f5d3d7fc6079661d52efb0593a67798e71b12fd8
Phuoc Quach ngoc <phuocquach.nt@gmail.com>
2020-11-08 16:50:21 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Overlay></Overlay>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Overlay from './components/Overlay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Overlay
+  }
+}
+</script>
